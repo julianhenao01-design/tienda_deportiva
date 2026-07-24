24fe82f2d565d8917e1e5ee551afc6dde3e9369d
julianhenao01-design <julian.henao01@unicatolica.edu.co>
2026-07-24 15:19:09 -0500

    fix: habilitar servicio de archivos media para logos en producción

--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,17 @@
 URL configuration for config project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tienda.urls')),  # Enlaza todas las rutas de la app tienda
+
+    # Permite a Django servir la carpeta /media/ (logos e imágenes) en producción/Railway
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
